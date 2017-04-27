700b402fbfcd5ad4a870f16888a5ddf74f6085c3
Brian Hsu <b55ntw@gmail.com>
2017-04-27 17:33:55 +0800

    add recycleview into StepViewActivity implemented card expand and collapse in StepViewAdapter

--- app/src/main/java/org/itri/bioreactor2/ui/StepViewActivity.java
+++ app/src/main/java/org/itri/bioreactor2/ui/StepViewActivity.java
@@ -2,16 +2,25 @@ package org.itri.bioreactor2.ui;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import org.itri.bioreactor2.R;
+import org.itri.bioreactor2.ui.adpater.AutoControlCardAdapter;
+import org.itri.bioreactor2.ui.adpater.StepCardAdapter;
 
 public class StepViewActivity extends AppCompatActivity {
     TextView textView;
+    String[] CardTitle = new String [] {"STEP 1", "STEP 2", "STEP 3", "STEP 4", "STEP 5", "STEP 6"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +28,8 @@ public class StepViewActivity extends AppCompatActivity {
         Log.d("test","onCreat StepViewActivity: " + getIntent().getStringExtra("StepList_Title"));
 
         setupToolBar();
-
+        setupFloatingActionButton();
+        setupRecyclerView();
 
     }
 
@@ -39,4 +49,32 @@ public class StepViewActivity extends AppCompatActivity {
         return false;
     }
 
+    private void setupFloatingActionButton(){
+        final View CoordinatorLayout = findViewById(R.id.CoordinatorLayout);
+        FloatingActionButton addCard = (FloatingActionButton)findViewById(R.id.fab);
+        addCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(CoordinatorLayout, "Add a Step", Snackbar.LENGTH_LONG).show();
+
+            }
+        });
+    }
+
+    private void setupRecyclerView(){
+
+        RecyclerView rv = (RecyclerView) findViewById(R.id.rv_recycler_view);
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        rv.setHasFixedSize(true);
+
+        StepCardAdapter adapter = new StepCardAdapter(CardTitle, this);
+        rv.setAdapter(adapter);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        rv.setLayoutManager(llm);
+
+        // specify an adapter (see also next example)
+        //mAdapter = new MyAdapter1(myDataset);
+        //mRecyclerView.setAdapter(mAdapter);
+    }
 }
---
+++ app/src/main/java/org/itri/bioreactor2/ui/adpater/StepCardAdapter.java
@@ -0,0 +1,108 @@
+package org.itri.bioreactor2.ui.adpater;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.itri.bioreactor2.R;
+import org.itri.bioreactor2.ui.StepViewActivity;
+import org.itri.bioreactor2.ui.tools.ExpandAndCollapseViewUtil;
+
+/**
+ * Created by A20356 on 2017/4/25.
+ */
+
+public class StepCardAdapter extends RecyclerView.Adapter<StepCardAdapter.MyViewHolder> {
+
+    private String[] mDataset;
+    Context context;
+
+
+    public StepCardAdapter(String[] myDataset, Context context){
+        mDataset = myDataset;
+        this.context = context;
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        public CardView mCardView;
+        public TextView mTextView;
+        public LinearLayout linearLayoutDetails;
+        public ImageView imageView;
+        private static final int DURATION = 250;
+
+        String[] mDataset = new String[]{};
+        Context context;
+        public MyViewHolder(View v, Context context, String[] mDataset){
+            super(v);
+            this.mDataset = mDataset;
+            this.context = context;
+            mCardView = (CardView) v.findViewById(R.id.card_view);
+            mTextView = (TextView) v.findViewById(R.id.tv_text);
+            linearLayoutDetails = (LinearLayout) v.findViewById(R.id.linearLayoutDetails);
+            imageView = (ImageView) v.findViewById(R.id.card_header_inner_expand_btn);
+            imageView.setOnClickListener(new View.OnClickListener(){
+                public void onClick(View v) {
+                    toggleDetails(v);
+                }
+            });
+
+
+        }
+
+        public void toggleDetails(View view) {
+            if (linearLayoutDetails.getVisibility() == View.GONE) {
+                ExpandAndCollapseViewUtil.expand(linearLayoutDetails, DURATION);
+                imageView.setImageResource(R.drawable.ic_expand_more_black_24dp);
+                rotate(-180.0f);
+            } else {
+                ExpandAndCollapseViewUtil.collapse(linearLayoutDetails, DURATION);
+                imageView.setImageResource(R.drawable.ic_expand_less_black_24dp);
+                rotate(180.0f);
+            }
+        }
+
+        private void rotate(float angle) {
+            Animation animation = new RotateAnimation(0.0f, angle, Animation.RELATIVE_TO_SELF, 0.5f,
+                    Animation.RELATIVE_TO_SELF, 0.5f);
+            animation.setFillAfter(true);
+            animation.setDuration(DURATION);
+            imageView.startAnimation(animation);
+        }
+
+    }
+
+
+    @Override
+    public StepCardAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_step, parent, false);
+        MyViewHolder vh = new MyViewHolder(v, context, mDataset);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position){
+        holder.mTextView.setText(mDataset[position]);
+
+
+
+
+    }
+
+    @Override
+    public int getItemCount() { return mDataset.length; }
+
+
+
+
+
+}
